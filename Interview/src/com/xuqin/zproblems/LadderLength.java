2aeeae8023200c7e53e036c7bfb4d7f665b3ed20
unknown <xuqin@.(none)>
2013-05-31 23:42:59 +0800

    LadderLength

---
+++ Interview/src/com/xuqin/zproblems/LadderLength.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.xuqin.zproblems;
+
+import java.util.HashSet;
+
+/**
+ * @author xuqin
+ *
+ * 
+ */
+public class LadderLength {
+	public int ladderLength(String start, String end, HashSet<String> dict) {
+		
+		
+		return 0;
+    }
+}
