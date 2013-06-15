a8e3e583dad03a3b72d8a2fe9c44e47f14264001
xuqin <xuqin@xuqin-PC>
2013-06-15 22:17:53 +0800

    add

---
+++ Interview/src/com/xuqin/zproblems/EditDistance.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.xuqin.zproblems;
+
+/**
+ * @author xuqin
+ *
+ * 
+ */
+public class EditDistance {
+	public int minDistance(String word1, String word2) {
+		if(word1.isEmpty() && word2.isEmpty()) {
+			return 0;
+		}
+		int [][] array = new int[word1.length()][word2.length()];
+		for(int i=0;i<word1.length();++i) {
+			for(int j=0;j<word2.length();++j) {
+				
+			}
+		}
+		
+		return 0;
+    }
+}
