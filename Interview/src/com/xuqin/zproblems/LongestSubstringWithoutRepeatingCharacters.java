393b8aead34d6cb6db0ba5855fc7590b44197602
xuqin <xuqin@xuqin-PC>
2013-06-28 11:22:33 +0800

    add

---
+++ Interview/src/com/xuqin/zproblems/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.xuqin.zproblems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author xuqin
+ *
+ * 
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+	 public int lengthOfLongestSubstring(String s) {
+		 if(s==null || s.length()==0) {
+			 return 0;
+		 }
+		 int max =  0;
+		 int [] table = new int[26];
+		 Map<Character,Integer> indexes = new HashMap<Character, Integer>();
+		 int start = 0;
+		 for(int i=0;i<s.length();++i) {
+			 Character c = s.charAt(i);
+			 if(table[c-'a']==0) {     //not exists
+				 table[c-'a']=1;
+				 indexes.put(c, i);
+				 max = Math.max(max,i-start+1);
+			 } else {              //exists
+				 
+				 start = indexes.get(c)+1;
+				 
+				 table[c-'a']=1;
+				 
+				 indexes.remove(c);
+				 indexes.put(c, i);
+				 
+				 max = Math.max(max,i-start+1);
+			 }
+		 }
+		 
+		 return max;
+	 }
+	 
+	 public static void main(String[] args) {
+		 LongestSubstringWithoutRepeatingCharacters longestSubstringWithoutRepeatingCharacters = new LongestSubstringWithoutRepeatingCharacters();
+		 System.out.println(longestSubstringWithoutRepeatingCharacters.lengthOfLongestSubstring("abcddcbae"));
+	 }
+}
---
+++ Interview/src/com/xuqin/zproblems/MedianOfTwoSortedArrays.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.xuqin.zproblems;
+
+/**
+ * @author xuqin
+ *
+ * 
+ */
+public class MedianOfTwoSortedArrays {
+	public double findMedianSortedArrays(int A[], int B[]) {
+		return 0.0;
+	}
+}
